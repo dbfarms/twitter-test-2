a737dd7827b5b6a5d0d38f79ad7d1633b28bd77c
dbfarms <matt.hunger@gmail.com>
2018-02-05 15:42:47 +0000

    initial redux setup

---
+++ twitter-test2-react/src/Reducers/SearchReducer.js
@@ -0,0 +1,10 @@
+export default function SearchReducer( state = {
+    searches: [],
+    editing: null,
+}, action) {
+    switch(action.type) {
+        
+        default: 
+            return state 
+    }
+}
--- twitter-test2-react/src/index.js
+++ twitter-test2-react/src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
+import rootReducer from './Reducers/Root';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
+
+const composeEnhancers = composeWithDevTools({});
+const store = createStore(
+  rootReducer,
+  composeEnhancers()
+);
+// applyMiddleware(thunk) <-- add into composeenhancers() maybe later if so desired
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+   document.getElementById('root'));
+registerServiceWorker();
+
+/*
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,3 +32,5 @@ ReactDOM.render(
   </BrowserRouter>, 
    document.getElementById('root'));
 registerServiceWorker();
+
+*/
